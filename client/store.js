e321d20229e07c12d7a10c03fd76d92e2a3175a1
Sylvia <sylviad1221@gmail.com>
2017-02-17 18:12:35 -0500

    add redux store

---
+++ client/store.js
@@ -0,0 +1,24 @@
+import { createStore, compose } from 'redux';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { browserHistory } from 'react-router';
+
+// import the root reducer
+import rootReducer from './reducers/index';
+
+import comments from './data/comments';
+import posts from './data/posts';
+
+// create an object for the default data
+// ES6 syntax makes it so you just have to write the name of the array in the object literal
+const defaultState = {
+  posts,
+  comments
+};
+
+
+const store = createStore(rootReducer, defaultState);
+
+// This allows react router to keep track of the browsing history from page to page
+export const history = syncHistoryWithStore(browserHistory, store);
+
+export default store;
