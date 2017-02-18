588097fd1a632a2cadab240b8a44d6a9876ce562
Sylvia <sylviad1221@gmail.com>
2017-02-17 20:24:54 -0500

    implementing store and history into routes

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -12,16 +12,20 @@ import Main from './components/Main';
 import Single from './components/Single';
 import PhotoGrid from './components/PhotoGrid';
 
-// import react router deps
+// import react router dependenciies
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Provider } from 'react-redux'; //This is the dependency that allows us to use redux with react
+import store, { history } from './store'; // import the modules from store
 
 const router = (
-  <Router history={browserHistory}>
-    <Route path="/" component={Main}>
-      <IndexRoute component={PhotoGrid}></IndexRoute>
-      <Route path="/view/:postId" component={Single}></Route>
-    </Route>
-  </Router>
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={Main}>
+        <IndexRoute component={PhotoGrid}></IndexRoute>
+        <Route path="/view/:postId" component={Single}></Route>
+      </Route>
+    </Router>
+  </Provider>
 )
 
 render(router, document.getElementById('root'));
